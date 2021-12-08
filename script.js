1a9a8fea4e676c4a5948e02bb9c79e0efbcf4e25
Sven Sonnborn <sonnborn.sven@web.de>
2021-12-08 03:04:56 +0100

    main

---
+++ script.js
@@ -0,0 +1,461 @@
+let questions_game = [
+    {
+        "question": "Das Online-Match beginnt. Welche Buchstaben werden normalerweise beim Start ausgetauscht?",
+        "answer_1": "gl hf",
+        "answer_2": "lol",
+        "answer_3": "hdgdl",
+        "answer_4": "thx",
+        "Right_answer": 1
+    },
+    {
+        "question": "In Gamer-Kreisen gibt es für Anfänger einen Begriff. Der lautet ....",
+        "answer_1": "mod",
+        "answer_2": "troll",
+        "answer_3": "imba",
+        "answer_4": "noob",
+        "Right_answer": 4
+    },
+    {
+        "question": "Wie sagt das man, dass man eine Weile nicht verfügbar ist?",
+        "answer_1": "fps",
+        "answer_2": "afk",
+        "answer_3": "nvm",
+        "answer_4": "gh",
+        "Right_answer": 2
+    },
+    {
+        "question": "Um jemanden zu loben schreibt man was?",
+        "answer_1": "gj",
+        "answer_2": "wtf",
+        "answer_3": "ffs",
+        "answer_4": "l2p",
+        "Right_answer": 1
+    },
+    {
+        "question": "Ein Spieler lauert immer an der gleichen Stelle. Wie nennt man solche Spieler?",
+        "answer_1": "quitter",
+        "answer_2": "camper",
+        "answer_3": "aimbot",
+        "answer_4": "duckjump",
+        "Right_answer": 2
+    }, {
+        "question": "In Multiplayer spielen taucht man für gewöhnlich nach dem man stirbt wieder auf. Wie lautet der Fachbegriff dafür?",
+        "answer_1": "spawnen",
+        "answer_2": "phoenixen",
+        "answer_3": "slaven",
+        "answer_4": "pubben",
+        "Right_answer": 1
+    },
+    {
+        "question": "Wie lautet der Begriff für Rollenspiele die man mit hunderten anderen Mitspielern gleichzeitig zockt?",
+        "answer_1": "FPS",
+        "answer_2": "RTS",
+        "answer_3": "MMORPG",
+        "answer_4": "MOBA",
+        "Right_answer": 3
+    }, {
+        "question": "Einer ihrer Mitspieler macht etwas dummes, aber entschuldigt sich dafür. Als nachsichtiger Mensch schreiben Sie:",
+        "answer_1": "np",
+        "answer_2": "rmk",
+        "answer_3": "ffs",
+        "answer_4": "omw",
+        "Right_answer": 1
+    },
+    {
+        "question": "Jemand braucht Hilfe. Sie sind nah am Geschehen. Bevor Sie loslaufen schreiben Sie:",
+        "answer_1": "ooc",
+        "answer_2": "rev",
+        "answer_3": "apm",
+        "answer_4": "omw",
+        "Right_answer": 4
+    },
+    {
+        "question": "Egal ob ein Spiel gewonnen oder verloren wird schreibt man am Ende eines Spieles:",
+        "answer_1": "gtfo",
+        "answer_2": "kk",
+        "answer_3": "gg",
+        "answer_4": "pwned",
+        "Right_answer": 3
+    },
+]
+let questions_prog = [
+    {
+        "question": "Welchen der folgenden Programiersprachen-Typen gibt es NICHT?",
+        "answer_1": "Compiler-Sprache",
+        "answer_2": "Interaktive-Sprache",
+        "answer_3": "Objektorientierte Programmiersprache",
+        "answer_4": "Skriptsprache",
+        "Right_answer": 1
+    },
+    {
+        "question": "Welche der folgenden Sprachen wird vor der Ausführung kompiliert?",
+        "answer_1": "JavaScript",
+        "answer_2": "C++",
+        "answer_3": "Batch",
+        "answer_4": "Python",
+        "Right_answer": 2
+    },
+    {
+        "question": "Eine switch case-Struktur ist im Verwendungszweck am ehesten verwandt mit...",
+        "answer_1": "While",
+        "answer_2": "Void",
+        "answer_3": "Class",
+        "answer_4": "if/else",
+        "Right_answer": 4
+    },
+    {
+        "question": "Ein Semikolon (;) ist in vielen Programmiersprachen wichtig, um eine Zeile/Anweisung abzuschließen. Nicht jedoch in...",
+        "answer_1": "C#",
+        "answer_2": "JavaScript",
+        "answer_3": "Python",
+        "answer_4": "C++",
+        "Right_answer": 3
+    },
+    {
+        "question": "Die meisten dieser Begriffe stehen in direkter Verbindung zu Funktionen. Welcher nicht?",
+        "answer_1": "Public/private",
+        "answer_2": "Rückgabewert",
+        "answer_3": "Argument",
+        "answer_4": "Destruktor",
+        "Right_answer": 4
+    }, {
+        "question": "Bei Java handelt es sich um eine Sprache, die...",
+        "answer_1": "...ohne Zusatzbibliotheken keine Objektorientierung unterstützt",
+        "answer_2": "...unter Linux nicht lauffähig ist",
+        "answer_3": "...besonders wegen ihrer hohen Geschwindigkeit eingesetzt wird",
+        "answer_4": "...zuerst vorkompiliert und später per Java-Plattform ausgeführt wird",
+        "Right_answer": 4
+    },
+    {
+        "question": "Unter einem struct versteht man...(u.A. aus C++ bekannt)",
+        "answer_1": "...eine Klasse ohne Methoden",
+        "answer_2": "...eine Variable ohne Inhalt",
+        "answer_3": "...ein mehrdimensionales Array",
+        "answer_4": "...eine Funktion/Methode ohne Argumente",
+        "Right_answer": 1
+    }, {
+        "question": "Im Unterschied zu vielen anderen Programmiersprachen verzichtet Python auf...",
+        "answer_1": "...Kaufmännisches Und && für logisches AND",
+        "answer_2": "...Eckige Klammern [] für Arrays",
+        "answer_3": "...Anführungszeichen für Strings",
+        "answer_4": "...geschweifte Klammern {} für Kontrollstrukturen Begründung: In Python werden statt den geschweiften Klammern die Einrückungen beachtet.",
+        "Right_answer": 4
+    },
+    {
+        "question": "Try/catch ist eine Struktur, die...",
+        "answer_1": "...die Entwicklung vereinfacht, indem Syntaxfehler schon im Editor erkannt werden",
+        "answer_2": "...Fehlerfälle in der Ausführung abfängt und sogenannte Exceptions wirft",
+        "answer_3": "...den Datentyp einer Variable mittels Ausprobieren von Operationen in Erfahrung bringt",
+        "answer_4": "...den Ablauf des Programms um eine beliebige Anzahl an Anweisungen rückgängig macht",
+        "Right_answer": 2
+    },
+    {
+        "question": "Werte aus Variablen lassen sich in PHP am einfachsten wie folgt in Strings einfügen:",
+        "answer_1": "...Beispielstring Nummer {}.format(num)",
+        "answer_2": "...Beispielstring Nummer %s num",
+        "answer_3": "...Beispielstring Nummer $num",
+        "answer_4": "...Beispielstring +num",
+        "Right_answer": 3
+    },
+]
+let questions_comp = [
+    {
+        "question": "Was bedeutet die Abkürzung DVD?",
+        "answer_1": "Digital Video Demand",
+        "answer_2": "Digital Visual Disc",
+        "answer_3": "Domaine Video Disc",
+        "answer_4": "Digital Versatile Dic",
+        "Right_answer": 4
+    },
+    {
+        "question": "Wie nennt man einen virtuellen Raum?",
+        "answer_1": "Internet",
+        "answer_2": "Cyberspace",
+        "answer_3": "World Wide Web",
+        "answer_4": "Raumzeit",
+        "Right_answer": 2
+    },
+    {
+        "question": "Wer ist der CEO von Microsoft?",
+        "answer_1": "Big Mate",
+        "answer_2": "Bill Gates",
+        "answer_3": "Steve Ballmer",
+        "answer_4": "Linus Torward",
+        "Right_answer": 3
+    },
+    {
+        "question": "1111 ist der Binärcode für?",
+        "answer_1": "16",
+        "answer_2": "14",
+        "answer_3": "15",
+        "answer_4": "12",
+        "Right_answer": 3
+    },
+    {
+        "question": "Welches dieser Begriffe ist ein Betriebssystem?",
+        "answer_1": "Photoshop",
+        "answer_2": "Excel",
+        "answer_3": "Windows",
+        "answer_4": "Netscape Navigator",
+        "Right_answer": 3
+    }, {
+        "question": "Was versteht man unter einem Broadcast?",
+        "answer_1": "Eine Meldung die alle Rechner in einem Netzwerk empfangen",
+        "answer_2": "Ein Datenpaket mit hoher Priorität",
+        "answer_3": "Einen Computervirus der sich von alleine über das Internet verbreitet",
+        "answer_4": "Eine serielle Netzwerverbindung mit 1000MBit/s",
+        "Right_answer": 1
+    },
+    {
+        "question": "Wie heißt ein Standart-Dateiformat, in dem moderne Digitalfotoapparate ihre Daten speichern?",
+        "answer_1": "Exig",
+        "answer_2": "Exit",
+        "answer_3": "Exip",
+        "answer_4": "Exif",
+        "Right_answer": 4
+    }, {
+        "question": "Was ist KEIN Algorithmus zum Sortieren von Werten",
+        "answer_1": "InsertionSort",
+        "answer_2": "EmbeddedSort",
+        "answer_3": "QuickSort",
+        "answer_4": "BubbleSort",
+        "Right_answer": 2
+    },
+    {
+        "question": "Wie heißt ein bekannter File Sharing Client",
+        "answer_1": "Nutella",
+        "answer_2": "Labello",
+        "answer_3": "Gnutella",
+        "answer_4": "Monello",
+        "Right_answer": 3
+    },
+    {
+        "question": "Wie nennt man die installierte Betriebssoftware von Geräten",
+        "answer_1": "Fixware",
+        "answer_2": "Firmware",
+        "answer_3": "Hardware",
+        "answer_4": "Shareware",
+        "Right_answer": 2
+    },
+]
+let quiz = [
+    {
+        "welcome": "Wilkommen zum Quiz der Zockersprache",
+        "gametype": questions_game,
+
+    },
+    {
+        "welcome": "Wilkommen zum Programmierquiz",
+        "gametype": questions_prog,
+    },
+    {
+        "welcome": "Wilkommen zum Computerquiz",
+        "gametype": questions_comp,
+    },
+]
+let right_answers = 0;
+let currentQuestion = 0;
+let questionnumber = 1;
+let which_game = [];
+let gametype;
+let correct_audio = new Audio('./sounds/correct.mp3');
+let wrong_audio = new Audio('./sounds/wrong.mp3');
+let lWidth = window.innerWidth;
+
+
+function init() {
+    document.getElementById('quizlength').innerHTML = which_game.length;
+    showQuestion();
+
+}
+
+function showQuestion() {
+
+    if (currentQuestion >= which_game.length) {
+        showendscreen();
+    } else {
+        updateprogress();
+        questionchange();
+    }
+}
+
+function answer(selection) {
+    let question = which_game[currentQuestion];
+    let selectedQuestionnumber = selection.slice(-1);
+    let idofrightanswer = `answer_${question['Right_answer']}`;
+
+    if (selectedQuestionnumber == question['Right_answer']) {
+        correct_audio.play();
+        document.getElementById(selection).parentNode.classList.add('bg-success');
+        right_answers++;
+    } else {
+        wrong_audio.play();
+        document.getElementById(selection).parentNode.classList.add('bg-danger');
+        document.getElementById(idofrightanswer).parentNode.classList.add('bg-success');
+    }
+    document.getElementById('nextquestion').classList.remove('disabled');
+}
+
+function nextquestion() {
+    currentQuestion++;
+    resetcolor();
+    document.getElementById('nextquestion').classList.add('disabled');
+    showQuestion();
+}
+function updateprogress(){
+    let percent = (currentQuestion + 1) / which_game.length;
+    percent = Math.round(percent * 100);
+    document.getElementById('progress-bar').innerHTML = `${percent}%`;
+    document.getElementById('progress-bar').style = `width : ${percent}%`;
+}
+function resetcolor() {
+    document.getElementById('answer_1').parentNode.classList.remove('bg-danger');
+    document.getElementById('answer_1').parentNode.classList.remove('bg-success');
+    document.getElementById('answer_2').parentNode.classList.remove('bg-danger');
+    document.getElementById('answer_2').parentNode.classList.remove('bg-success');
+    document.getElementById('answer_3').parentNode.classList.remove('bg-danger');
+    document.getElementById('answer_3').parentNode.classList.remove('bg-success');
+    document.getElementById('answer_4').parentNode.classList.remove('bg-danger');
+    document.getElementById('answer_4').parentNode.classList.remove('bg-success');
+}
+function load(id) {
+    document.getElementById('welcome').innerHTML = `${quiz[id]['welcome']}`;
+    document.getElementById('card-text').innerHTML = `Ready for the challange?`;
+    document.getElementById('startbutton').innerHTML = `
+    <button type="button startbutton" class="btn btn-primary" onclick="start(${id})">START NOW</button>
+    `;
+    document.getElementById('startbutton').innerHTML = `
+    <button type="button startbutton" class="btn btn-primary" onclick="start(${id})">START NOW</button>
+    `;
+    gametype = quiz[id]['gametype'];
+    activelink(id);
+}
+function start(id) {
+    document.getElementById('questionbody').style = '';
+    document.getElementById('welcomescreen').style = 'display : none !important';
+    document.getElementById('progress').style = '';
+    document.getElementById('progress-bar').innerHTML = `0%`;
+    which_game = gametype;
+    init();
+    adddisplay();
+    if (lWidth < 960) {
+        document.getElementById('left').classList.add('maxH');
+    }
+}
+function screensize() {
+    lWidth = window.innerWidth;
+    if (lWidth < 960) {
+        console.log('Less than 960');
+        document.getElementById('link-list').classList.remove('mt-5');
+        document.getElementById('card2').classList.remove('mb-3');
+    }
+    else {
+        document.getElementById('link-list').classList.add('mt-5');
+        document.getElementById('card2').classList.add('mb-3');
+    }
+}
+function activelink(id) {
+    if (id <= 0) {
+        document.getElementById('quizlink1').classList.add('active');
+        document.getElementById('quizlink2').classList.remove('active');
+        document.getElementById('quizlink3').classList.remove('active');
+        replay2();
+    } else
+        if (id <= 1) {
+            document.getElementById('quizlink2').classList.add('active');
+            document.getElementById('quizlink1').classList.remove('active');
+            document.getElementById('quizlink3').classList.remove('active');
+            replay2();
+        } else
+            if (id <= 2) {
+                document.getElementById('quizlink3').classList.add('active');
+                document.getElementById('quizlink1').classList.remove('active');
+                document.getElementById('quizlink2').classList.remove('active');
+                replay2();
+            }
+}
+function removedisplay(id){
+    if (lWidth < 960) {
+        if (id < 1) {
+            document.getElementById('quizlink2').classList.remove('d-none');
+            document.getElementById('quizlink3').classList.remove('d-none');
+        } else {
+            if (id == 1) {
+                document.getElementById('quizlink1').classList.remove('d-none');
+                document.getElementById('quizlink3').classList.remove('d-none');
+            }
+            else {
+                document.getElementById('quizlink2').classList.remove('d-none');
+                document.getElementById('quizlink1').classList.remove('d-none');
+            }
+        }
+    }
+}
+function adddisplay(id){
+    if (lWidth < 960) {
+        if (id < 1) {
+            document.getElementById('quizlink2').classList.add('d-none');
+            document.getElementById('quizlink3').classList.add('d-none');
+        } else {
+            if (id == 1) {
+                document.getElementById('quizlink1').classList.add('d-none');
+                document.getElementById('quizlink3').classList.add('d-none');
+            }
+            else {
+                document.getElementById('quizlink2').classList.add('d-none');
+                document.getElementById('quizlink1').classList.add('d-none');
+            }
+        }
+    }
+}
+function questionchange(){
+    let question = which_game[currentQuestion];
+    document.getElementById('questionnumber').innerHTML = currentQuestion + 1;
+    document.getElementById('questiontext').innerHTML = question['question'];
+    document.getElementById('answer_1').innerHTML = question['answer_1'];
+    document.getElementById('answer_2').innerHTML = question['answer_2'];
+    document.getElementById('answer_3').innerHTML = question['answer_3'];
+    document.getElementById('answer_4').innerHTML = question['answer_4'];
+}
+function replay(id) {
+    right_answers = 0;
+    currentQuestion = 0;
+    questionnumber = 1;
+    document.getElementById('quiz1').classList.remove('final-mobile');
+    document.getElementById('endScreen').style = 'display : none';
+    document.getElementById('welcomescreen').style = '';
+    remove();
+    removedisplay();
+    if (lWidth < 960) {
+        document.getElementById('left').classList.remove('maxH');
+    }
+}
+function replay2() {
+    right_answers = 0;
+    currentQuestion = 0;
+    questionnumber = 1;
+    resetcolor();
+    document.getElementById('endScreen').style = 'display : none';
+    document.getElementById('questionbody').style = 'display : none';
+    document.getElementById('progress').style = 'display : none';
+    document.getElementById('welcomescreen').style = '';
+}
+function remove() {
+    let d = document.getElementById('quiz1');
+    let d_nested = document.getElementById('trophy');
+    let throwawayNode = d.removeChild(d_nested);
+}
+function showendscreen(){
+    document.getElementById('endScreen').style = '';
+    document.getElementById('quiz1').classList.add('final-mobile');
+    document.getElementById('questionbody').style = 'display : none';
+    document.getElementById('progress').style = 'display : none';
+    document.getElementById('quizlength_answer').innerHTML = '';
+    document.getElementById('quizlength_answer').innerHTML = which_game.length;
+    document.getElementById('rightanswers').innerHTML = '';
+    document.getElementById('rightanswers').innerHTML = right_answers;
+    document.getElementById('quiz1').innerHTML +=
+        `
+    <img src="./img/tropy.png" class="trophy" id="trophy">
+    `;
+}
